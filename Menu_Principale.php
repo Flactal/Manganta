63fba45394dda626c348fd8ee2ef554a79d2342b
Samyn Axel <brinx@live.be>
2015-02-11 18:04:05 +0100

    Suppression des variables
    Les variables supprimées sont désormais accessible via Variables.php

--- Menu_Principale.php
+++ Menu_Principale.php
@@ -1,29 +1,3 @@
-<?php
-	/*=== Maintenance ===*/
-	$i;
-	
-	$nombre_Onglets = 4;
-	$nombre_Colonnes_Disponible_Onglets = 12;
-	$taille_Colonnes = 2; /* Valeur choisie par défaut */
-	$taille_Offset_Colonnes = $nombre_Colonnes_Disponible_Onglets - ($nombre_Onglets * $taille_Colonnes);
-	
-	/** Noms des onglets **/
-	$vecteur_Onglets = array();
-	
-	$vecteur_Onglets[0] = "Animes";
-	$vecteur_Onglets[1] = "Mangas";
-	$vecteur_Onglets[2] = "Base de données";
-	$vecteur_Onglets[3] = "Connexion";
-	
-	
-	$vecteur_Href_Onglets = array();
-	
-	$vecteur_Href_Onglets[0] = "Animes.php";
-	$vecteur_Href_Onglets[1] = "Mangas.php";
-	$vecteur_Href_Onglets[2] = "BDD.php";
-	$vecteur_Href_Onglets[3] = "Connexion.php";
-?>
-
 <nav id="Menu_Principale" class="row">
 	<!-- Titre Site -->
 	<div class="col-lg-2 navbar-header">
