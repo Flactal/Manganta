b44c8cf631af8e52b5c89a2703a3fa4a4e037a2d
Samyn Axel <brinx@live.be>
2015-02-11 16:53:14 +0100

    Uploaded Base Files
    Mise en ligne des fichiers déjà existants.

---
+++ Accueil.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Accueil</title>
+		
+		<meta charset="utf-8" />
+		
+		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="General_Stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="Accueil_Stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="Menu_Principale_Stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="Header_Stylesheet.css">
+	</head>
+	
+	<body class="container-fluid">
+		<!-- Menu Principale -->
+		<?php
+			include_once 'Menu_Principale.php';
+		?>
+		
+		<!-- Header -->
+		<?php
+			include_once 'Header.php';
+		?>
+		
+	</body>
+</html>
---
+++ Menu_Principale.php
@@ -0,0 +1,15 @@
+<nav id="Menu_Principale" class="row">
+	<!-- Titre Site -->
+	<div class="col-lg-2 navbar-header">
+		<a id="Brand_Name" class="navbar-brand">Manganta</a>
+	</div>
+	
+	<!-- Menu -->
+	<ul class="col-lg-10 nav navbar-nav">
+		<li class="col-lg-offset-2 col-lg-2"><a>Cocktails	</a></li>
+		<li class="col-lg-2">				<a>Alcools		</a></li>
+		<li class="col-lg-2">				<a>Bis		</a></li>
+		<li class="col-lg-2">				<a>Jeux			</a></li>
+		<li class="col-lg-2">				<a>Bons plans	</a></li>
+	</ul>
+</nav>
