b56ef2eba828ec3559677cd7d8a46bc3bac704d0
Samyn Axel <brinx@live.be>
2015-02-15 15:47:01 +0100

    Changement emplacement variables
    J'ai déplacé les variables images du fichiers header à variables.

--- Variables.php
+++ Variables.php
@@ -12,7 +12,7 @@
 	
 	$vecteur_Onglets[0] = "Animes";
 	$vecteur_Onglets[1] = "Mangas";
-	$vecteur_Onglets[2] = "Base de données";
+	$vecteur_Onglets[2] = "Base_de_données";
 	$vecteur_Onglets[3] = "Connexion";
 	
 	
@@ -23,5 +23,22 @@
 	$vecteur_Href_Onglets[2] = "BDD.php";
 	$vecteur_Href_Onglets[3] = "Connexion.php";
 	/** FIN MENU **/
+	
+	/* Chemin d'acces au images */
+	$dossier_Images = "Images/";
+	$sous_Dossier_Images = "Site/";
+	
+	$vecteur_Titre_Images = array();
+	
+	$vecteur_Titre_Images[0] = "nisekoi.png";
+	$vecteur_Titre_Images[1] = "spice_and_wolf.jpg";
+	$vecteur_Titre_Images[2] = "no_game_no_life.jpg";
+	
+	$vecteur_Description_Images = array();
+	
+	$vecteur_Description_Images[0] = "Liens_Animes";
+	$vecteur_Description_Images[1] = "Liens_Mangas";
+	$vecteur_Description_Images[2] = "Liens_Base_de_données";
+	/** FIN IMAGES **/
 	
 ?>
