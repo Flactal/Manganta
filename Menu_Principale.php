20c73d68fe802b920accc17397e9f2390a345f3d
Samyn Axel <brinx@live.be>
2015-02-17 22:33:07 +0100

    Responsive Design Maj

--- Menu_Principale.php
+++ Menu_Principale.php
@@ -6,27 +6,29 @@
 	$taille_Offset_Colonnes = $nombre_Colonnes_Disponible_Onglets - ($nombre_Onglets * $taille_Colonnes);
 ?>
 
-<nav id="Menu_Principale" class="row">
-	<!-- Titre Site -->
-	<div class="col-lg-2 navbar-header">
-		<a id="Brand_Name" class="navbar-brand" href="Accueil.php">Manganta</a>
-	</div>
-	
-	<!-- Menu -->
-	<ul class="col-lg-10 nav navbar-nav">
-		<!-- offset si nécessaire -->
-		<li <?php echo "class=col-lg-".$taille_Offset_Colonnes; ?>></li>
+<div class="col-lg-12">
+	<nav id="Menu_Principale" class="row">
+		<!-- Titre Site -->
+		<div class="col-lg-2 navbar-header">
+			<a id="Brand_Name" class="navbar-brand" href="Accueil.php">Manganta</a>
+		</div>
 		
-		<!-- Affichage des éléments contenu dans vecteur_Onglets -->
-		<?php
-			for($i = 0; $i < $nombre_Onglets; $i++)
-			{
-				?>
-				<li <?php echo "class=col-lg-".$taille_Colonnes; ?>>
-					<a <?php echo "href=".$vecteur_Href_Onglets[$i]; ?>> <?php echo $vecteur_Onglets[$i]; ?> </a>
-				</li>
-				<?php
-			}
-		?>
-	</ul>
-</nav>
+		<!-- Menu -->
+		<ul class="col-lg-10 nav navbar-nav">
+			<!-- offset si nécessaire -->
+			<li <?php echo "class=col-lg-".$taille_Offset_Colonnes; ?>></li>
+			
+			<!-- Affichage des éléments contenu dans vecteur_Onglets -->
+			<?php
+				for($i = 0; $i < $nombre_Onglets; $i++)
+				{
+					?>
+					<li <?php echo "class=col-lg-".$taille_Colonnes; ?>>
+						<a <?php echo "href=".$vecteur_Href_Onglets[$i]; ?>> <?php echo $vecteur_Onglets[$i]; ?> </a>
+					</li>
+					<?php
+				}
+			?>
+		</ul>
+	</nav>
+</div>
